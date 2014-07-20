12c0c569176c3a63fdebffd52350f86cfbb1e943
LEOMZE <lion_1393@mail.ru>
2014-07-20 10:54:41 +0400

    Add zero interface

---
+++ ManagerServer/src/main/java/IManagerServerDAO.java
@@ -0,0 +1,34 @@
+
+
+public interface IManagerServerDAO {
+
+    void insertUser();
+    void selectUser();
+    void removeUser();
+    void updateUser();
+
+    void insertEent();
+    void selectEvent();
+    void removeEvent();
+    void updateEvent();
+
+    void insertGroup();
+    void selectGrouo();
+    void removeGroup();
+    void updateGroup();
+
+    void insertComment();
+    void selectComment();
+    void removeComment();
+    void updateComment();
+
+    //or just
+
+    void insert(String str);//parse str
+    void select(String str);
+    void remove(String str);
+    void update(String str);
+
+
+
+}
