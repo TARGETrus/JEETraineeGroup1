261b1b09ac67bbdc1cf9d18007e75891a3cf1f99
TARGETrus <mousebreaker@mail.ru>
2014-07-22 16:24:26 +0400

    #0.5 hibernate

--- ManagerServer/src/main/java/DAO/ServerDAO.java
+++ ManagerServer/src/main/java/DAO/ServerDAO.java
@@ -8,17 +8,17 @@ import model.User;
 public interface ServerDAO {
 
     boolean insertUser(User user);
-    User selectUser(int userID);// что-то мне кажется селект по АЙДИ не очень удобным...
+    User    selectUser(int userID); // Предложения?
     boolean removeUser(User user);
     boolean updateUser(User user);
 
     boolean insertEvent(Event event);
-    Event selectEvent(int eventID);
+    Event   selectEvent(int eventID);
     boolean removeEvent(Event event);
     boolean updateEvent(Event event);
 
     boolean insertGroup(Group group);
-    Group selectGrouo(int groupID);
+    Group   selectGrouo(int groupID);
     boolean removeGroup(Group group);
     boolean updateGroup(Group group);
 
