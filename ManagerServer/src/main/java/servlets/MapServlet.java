9238a9fc4cf9d0db0a5bef7d8f5609fc8de128f3
Хвёдор <katrin4@mail.ru>
2014-08-01 15:49:08 +0400

    map test

--- ManagerServer/src/main/java/servlets/MapServlet.java
+++ ManagerServer/src/main/java/servlets/MapServlet.java
@@ -1,5 +1,9 @@
 package servlets;
 
+import aDAOTest.TestManager;
+import model.Event;
+import model.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -11,11 +15,18 @@ import java.io.IOException;
 @WebServlet("/MapServlet")
 public class MapServlet extends HttpServlet {
 
+    TestManager testManager = new TestManager();
+    User user;
+    Event event;
+
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=utf-8");
 
-        resp.addCookie(new Cookie("latitude", req.getParameter("lat")));
-        resp.addCookie(new Cookie("longitude", req.getParameter("long")));
+        user = testManager.findUserById(Integer.parseInt(req.getParameter("user_id")));
+        event = user.getEvents().iterator().next(); //get one user's event
+
+        resp.addCookie(new Cookie("latitude", event.getLatitude()));
+        resp.addCookie(new Cookie("longitude", event.getLongitude()));
 
         resp.sendRedirect("map.jsp");
     }
