a3b110076bc0fc3d91f589676205d6f6397832a9
Хвёдор <katrin4@mail.ru>
2014-08-02 16:45:22 +0400

    map test localhost:8080/map?user_id=1

--- ManagerServer/src/main/java/servlets/MapServlet.java
+++ ManagerServer/src/main/java/servlets/MapServlet.java
@@ -11,22 +11,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/MapServlet")
 public class MapServlet extends HttpServlet {
 
     TestManager testManager = new TestManager();
     User user;
-    Event event;
+    List<Event> events;
+    String lat = "";
+    String lon = "";
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=utf-8");
 
         user = testManager.findUserById(Integer.parseInt(req.getParameter("user_id")));
-        event = user.getEvents().iterator().next(); //get one user's event
+        events = new ArrayList<>(user.getEvents());
+        for(int i = 0; i < events.size(); i++) {
+            lat += events.get(i).getLatitude() + " ";
+            lon += events.get(i).getLongitude() + " ";
+        }
 
-        resp.addCookie(new Cookie("latitude", event.getLatitude()));
-        resp.addCookie(new Cookie("longitude", event.getLongitude()));
+        resp.addCookie(new Cookie("latitude", lat));
+        resp.addCookie(new Cookie("longitude", lon));
+
+        System.out.println(lat + "    +++   " + lon);
 
         resp.sendRedirect("map.jsp");
     }
