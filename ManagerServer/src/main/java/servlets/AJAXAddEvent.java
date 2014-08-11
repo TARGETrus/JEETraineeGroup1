feff385064d2a990b3b8b1cfc2cc919675bce846
LEOMZE <lion_1393@mail.ru>
2014-08-11 14:56:25 +0400

    Add hibernate normal encode to db

--- ManagerServer/src/main/java/servlets/AJAXAddEvent.java
+++ ManagerServer/src/main/java/servlets/AJAXAddEvent.java
@@ -26,13 +26,13 @@ public class AJAXAddEvent extends HttpServlet {
         String eventName = req.getParameter("eventName");
         String date = req.getParameter("date");
         date = date.replace("T", " ");
-        Float lng = 100F; //req.getParameter("lng");
-        Float lat = 100F;//req.getParameter("lat");
+        Float lng = Float.parseFloat(req.getParameter("lng"));
+        Float lat = Float.parseFloat(req.getParameter("lat"));
         String address = req.getParameter("coord");
 
         String str = null;
 
-        if(!date.equals(null) && !eventName.equals(null) && lng.equals(null) && lat.equals(null)){
+        if(!date.equals(null) && !eventName.equals(null) && !lng.equals(null) && !lat.equals(null)){
             JSONObject obj = new JSONObject();
             obj.put("name", "add_event");
             str = obj.toJSONString();
